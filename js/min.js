5bde24f6e2b70e30b57e4dc3fe16b0a712ea4deb
kenkuang1213 <kenkuang1213@gmail.com>
2015-09-30 02:59:14 +0800

    init

---
+++ js/min.js
@@ -0,0 +1,8 @@
+/*!
+ * reveal.js 1.0.0 (2015-09-29, 21:59)
+ * http://lab.hakim.se/reveal-js
+ * MIT licensed
+ *
+ * Copyright (C) 2015 Hakim El Hattab, http://hakim.se
+ */
+var app=angular.module("AboutMe",["pascalprecht.translate"]);app.config(function(a){});
